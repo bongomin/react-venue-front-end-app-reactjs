304c759a92454dc376858dfff032928e44e7cc66
bongomin <candanobongomindaniel@gmail.com>
2020-02-17 18:36:11 +0300

    feat(CountDown):
    - created a countDown Component that generate deadline,days,hours and minutes

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import Carrousel from './Carrousel';
+import CountDown from './CountDown'
 
 
 const Featured = () => {
    return (
       <div style={{ position: "relative" }}>
          <Carrousel />
-         
+
          <div className="artist_name">
             <div className="wrapper">
                Jose Chameleone
             </div>
          </div>
+         <CountDown />
 
       </div>
 
