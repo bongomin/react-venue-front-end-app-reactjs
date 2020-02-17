9699c0cf7ee231a8525cf7aaa77c81a42426cc68
bongomin <candanobongomindaniel@gmail.com>
2020-02-17 14:42:06 +0300

    feat(created a slider)
    - used the react-slick third party module
    
    - created a Carrousel component

---
+++ src/components/Featured/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Carrousel from './Carrousel';
+
+
+const Featured = () => {
+   return (
+      <div style={{ position: "relative" }}>
+         <Carrousel />
+         
+         <div className="artist_name">
+            <div className="wrapper">
+               Jose Chameleone
+            </div>
+         </div>
+
+      </div>
+
+   )
+
+}
+export default Featured;
