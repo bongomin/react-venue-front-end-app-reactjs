121b41212bd96be2f171c99d002c4ba1465595d8
bongomin <candanobongomindaniel@gmail.com>
2020-02-18 17:47:46 +0300

    feat(Adding Scroller):
    - Added a scroller to all side wrappers
    
    - used the react-scroll third party parckage for react

--- src/components/Header_Footer/SideBar.js
+++ src/components/Header_Footer/SideBar.js
@@ -2,10 +2,23 @@ import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+// importing third barty library for scrolling effect
+import { scroller } from 'react-scroll'
+
 
 
 
 const SideDrawer = (props) => {
+   const scrollToElement = (element) => {
+      scroller.scrollTo(element, {
+         duration: 1500,
+         delay: 500,
+         smooth: true,
+         offset: -150
+      })
+      props.onClose(false)
+
+   }
    return (
       <Drawer
          anchor="right"
@@ -13,16 +26,22 @@ const SideDrawer = (props) => {
          onClose={() => props.onClose(false)}
       >
          <List component="nav">
-            <ListItem button onClick={() => console.log('featured')}>
+            <ListItem button onClick={() => scrollToElement("eventStart")}>
                Events starts in
             </ListItem>
 
-            <ListItem button onClick={() => console.log('Venue Info')}>
+            <ListItem button onClick={() => scrollToElement("venueInfo")}>
                Venue Info
             </ListItem>
+            <ListItem button onClick={() => scrollToElement("pricing")}>
+               Pricing
+            </ListItem>
 
-            <ListItem button onClick={() => console.log('High Lights')}>
+            <ListItem button onClick={() => scrollToElement("highLights")}>
                High Lights
+            </ListItem>
+            <ListItem button onClick={() => scrollToElement("location")}>
+               Location
             </ListItem>
          </List>
       </Drawer>
