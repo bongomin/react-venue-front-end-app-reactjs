af2c951d79789e739d547c276fd2a9aec2572997
bongomin <candanobongomindaniel@gmail.com>
2020-02-18 14:05:36 +0300

    feat(Shared buttons): created a button component.
    - the Button Component takes in the props attribute becouse the component will be used in many other components for use.

--- src/components/HighLights/Discounts.js
+++ src/components/HighLights/Discounts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Fade from 'react-reveal/Fade'
 import Slide from 'react-reveal/Slide'
+import Button from '../utils/Button'
 
 export class Discounts extends Component {
    state = {
@@ -40,10 +41,17 @@ export class Discounts extends Component {
                <Slide right>
                   <div className="discount_description">
                      <h3>Purchase Tickets before end of week</h3>
-                     <p>here goes the descripiotions all..</p>
-                     <div>
-                        button
-                  </div>
+                     <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                     </p>
+
+                     <Button
+                        text="Purchase Tickets"
+                        bck="#ffa800"
+                        color="#ffffff"
+                        link="http://google.com"
+                     />
+
                   </div>
 
                </Slide>
